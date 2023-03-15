546ff02751fc20700c20a8455603d0c1b4f01270
fmqonik <kaniafajriya@gmail.com>
2023-03-15 15:21:22 +0700

    feat: created List Homestay Page and Daftar Upload Page

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import imgAuth from "assets/img-auth.webp";
-import imgLogo from "../../assets/Logo.webp";
+import imgLogo from "assets/Logo.webp";
 
 import { Link } from "react-router-dom";
 import Input from "components/Input";
--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import imgAuth from "assets/img-auth.webp";
-import imgLogo from "../../assets/Logo.webp";
+import imgLogo from "assets/Logo.webp";
 
 import Input from "components/Input";
 import Button from "components/Button";
